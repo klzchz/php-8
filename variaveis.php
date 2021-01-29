11d31184b8a0107855881b173d35e7021760226f
Lucas Souza de Andrade <lucascorporation@live.com>
2021-01-29 15:49:33 -0300

    Variaveis PHP-8

---
+++ variaveis.php
@@ -0,0 +1,20 @@
+<?php
+
+
+$primeiroNome = "Lucas";
+$ultimoNome = "Souza de Andrade";
+
+$nomeCompleto = $primeiroNome . " " . $ultimoNome;
+
+/*
+echo $primeiroNome;
+echo "<br/>";
+echo $ultimoNome;
+echo "<br/>";
+ */
+//echo strlen($nomeCompleto);
+
+
+var_dump($primeiroNome);
+var_dump($ultimoNome);
+var_dump($nomeCompleto);
