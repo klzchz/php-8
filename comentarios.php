d2af5626cefdee1a059c09a232a1b4c5421cac07
Lucas Souza de Andrade <lucascorporation@live.com>
2021-01-29 15:43:47 -0300

    Comentários PHP-8

---
+++ comentarios.php
@@ -0,0 +1,25 @@
+<?php
+
+//Este é um comentário de linha
+
+//echo "Hcode";
+
+/*
+    function somar() {
+        return 2 + 2;
+
+    }
+*/
+
+/**
+ * Normal
+ * * Comentário destacado
+ * ! COmentário de alerta
+ * ? Dúvida
+ * TODO: A fazer
+ * @param nome Informar o nome da pessoa
+ *
+ *
+ *
+ *
+ */
